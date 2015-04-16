1e039563e87dabad31e0c3be698529ca015be1a3
claylin <claylin.007@gmail.com>
2015-04-16 11:02:26 +0800

    Search function added, page 490.

--- app/src/main/java/com/example/a001/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/example/a001/photogallery/FlickrFetchr.java
@@ -20,10 +20,14 @@ import java.util.ArrayList;
  */
 public class FlickrFetchr {
     public static final String TAG = "FlickrFetchr";
+    public static final String PREF_SEARCH_QUERY = "searchQuery";
+
     private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
     private static final String API_KEY = "8e3b10b5490dc7124627ba8be891b776";   //Secret for this key: 1c44e6cb9f1f7efe
     private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+    private static final String METHOD_SEARCH = "flickr.photos.search";
     private static final String PARAM_EXTRAS = "extras";
+    private static final String PARAM_TEXT = "text";
     private static final String EXTRA_SMALL_URL = "url_s";
 
     private static final String XML_PHOTO = "photo";
@@ -57,16 +61,10 @@ public class FlickrFetchr {
         return new String(getUrlBytes(urlSpec));
     }
 
-    public ArrayList<GalleryItem> fetchItems() {
+    public ArrayList<GalleryItem> downloadGalleryItems(String  url) {
         ArrayList<GalleryItem> items = new ArrayList<GalleryItem>();
 
         try {
-            String url =
-                    Uri.parse(ENDPOINT).buildUpon()
-                    .appendQueryParameter("method", METHOD_GET_RECENT)
-                    .appendQueryParameter("api_key", API_KEY)
-                    .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
-                    .build().toString();
             String xmlString = getUrl(url);
             //String xmlString = getUrl("https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=8e3b10b5490dc7124627ba8be891b776&extras=url_s");
             Log.i(TAG, "Received xml: " + xmlString);
@@ -81,6 +79,26 @@ public class FlickrFetchr {
             Log.e(TAG, "Failed to parse items", xppe);
         }
         return items;
+    }
+
+    public ArrayList<GalleryItem> fetchItems() {
+        // Move code here from above
+        String url = Uri.parse(ENDPOINT).buildUpon()
+                        .appendQueryParameter("method", METHOD_GET_RECENT)
+                        .appendQueryParameter("api_key", API_KEY)
+                        .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+                        .build().toString();
+        return downloadGalleryItems(url);
+    }
+
+    public ArrayList<GalleryItem> search(String query) {
+        String url = Uri.parse(ENDPOINT).buildUpon()
+                .appendQueryParameter("method", METHOD_SEARCH)
+                .appendQueryParameter("api_key", API_KEY)
+                .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+                .appendQueryParameter(PARAM_TEXT, query)
+                .build().toString();
+        return downloadGalleryItems(url);
     }
 
     void parseItems(ArrayList<GalleryItem> items, XmlPullParser parser) throws XmlPullParserException, IOException {
